56bd5bef0ab5ac65e8cd89f6a5208e016e2c1151
king-oyster-pumpkin-soup-seasonings <283332318+king-oyster-pumpkin-soup-seasonings@users.noreply.github.com>
2026-08-13 19:00:55 +0800

    feat(camera):  implement camera following object, also follow mouse hover on right side

---
+++ Assets/Scripts/Cameraman.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+public class Cameraman : MonoBehaviour
+{
+    [SerializeField] private Transform target, targetArea;
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] private float cameraSmoothSpeed;
+    [SerializeField] private Vector3 mousePositionScreen;
+    private float originX, fixedY, fixedZ;
+    private bool enableCameraFollow;
+    private Rigidbody2D cameraRB;
+
+    private Camera cam;
+
+    private void OnEnable() => Slingshot.ObjectLaunched += SetCameraFollow;
+    private void OnDisable() => Slingshot.ObjectLaunched -= SetCameraFollow;
+
+
+    void SetCameraFollow(bool state = true)
+    {
+        enableCameraFollow = state;
+    }
+
+    void Start()
+    {
+        enableCameraFollow = false;
+        if (cameraSmoothSpeed == 0) cameraSmoothSpeed = 5f;
+        if (minX == 0) minX = 0f;
+        if (maxX == 0) maxX = 21f;
+
+        originX = 0;
+        fixedY = transform.position.y;
+        fixedZ = transform.position.z;
+
+        cam = GetComponent<Camera>();
+        cameraRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void LateUpdate()
+    {
+        if (target == null) return;
+
+        mousePositionScreen = Input.mousePosition;
+        if (!enableCameraFollow)
+        {
+            if (mousePositionScreen.x >= 1671) MoveCameraWithLerp(targetArea);
+            // else MoveCameraWithLerp(GetOriginTransform());
+            else
+            {
+                Vector3 clampedTargetPosition =
+                    new Vector3(Mathf.Clamp(0, minX, maxX), fixedY, fixedZ);
+                transform.position =
+                    Vector3.Lerp(transform.position, clampedTargetPosition, Time.deltaTime * cameraSmoothSpeed);
+            }
+        }
+        else MoveCameraWithLerp(target);
+        // transform.position = new Vector3(target.position.x, fixedY, fixedZ);
+
+
+        cam.orthographicSize = Mathf.Max(5f,
+            5f + (target.position.x * 0.05f) + (target.position.y * 0.4f));
+    }
+
+    Transform GetOriginTransform()
+    {
+        Transform modifiedTransform = transform;
+        modifiedTransform.position = new Vector3(originX, fixedY, fixedZ);
+        return modifiedTransform;
+    }
+
+    void MoveCameraWithLerp(Transform targetPoint)
+    {
+        Vector3 clampedTargetPosition =
+            new Vector3(Mathf.Clamp(targetPoint.position.x, minX, maxX), Mathf.Clamp(targetPoint.position.y, 0, 5f),
+                fixedZ);
+        transform.position =
+            Vector3.Lerp(transform.position, clampedTargetPosition, Time.deltaTime * cameraSmoothSpeed);
+    }
+}
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -1,29 +1,88 @@
+using System;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
-    private Rigidbody2D slingyRB;
+    [SerializeField] private Vector2 mousePositionVec2;
+    [SerializeField] private float force;
+    [SerializeField] private Vector2 restingPointVec2, dragPointVec2;
+    private Rigidbody2D objectRB;
     private bool isReady;
-    private float timeCounter, timeIdlePosition;
+    private float idleTimeCounter, timeIdlePosition;
+
+    public static Action<bool> ObjectLaunched;
 
     void Start()
     {
         InitializeVariables();
-        ResetReady();
+        PositionAndReady();
     }
 
     void Update()
     {
+        mousePositionVec2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        if (objectRB.position.x > 40 || objectRB.position.x < -15 || objectRB.position.y < -10) ResetPosition();
+        else if (objectRB.linearVelocity is { x: < 2, y: < 2 })
+        {
+            idleTimeCounter += Time.deltaTime;
+        }
+        else idleTimeCounter = 0;
+
+        if (idleTimeCounter >= 3) ResetPosition();
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!isReady) return;
+
+        transform.position = mousePositionVec2;
+        dragPointVec2 = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isReady) return;
+
+        PositionAndReady();
+    }
+
+    private void OnMouseUp()
+    {
+        if (!isReady) return;
+
+        idleTimeCounter = 0;
+        isReady = false;
+        objectRB.gravityScale = 1f;
+        objectRB.linearVelocity = (restingPointVec2 - dragPointVec2) * force;
+        ObjectLaunched?.Invoke(true);
     }
 
     void InitializeVariables()
     {
-        slingyRB = GetComponent<Rigidbody2D>();
+        // SFs
+        if (force == 0) force = 1f;
+
+        // Non-SF
+        isReady = true;
+        objectRB = GetComponent<Rigidbody2D>();
+        restingPointVec2 = new Vector2(-6.75f, -2.5f);
     }
 
-    void ResetReady()
+    void PositionAndReady()
     {
-        slingyRB.gravityScale = 0;
-        transform.position = new Vector2(-6.75f, -2.5f);
+        objectRB.gravityScale = 0;
+        transform.position = restingPointVec2;
+    }
+
+    void ResetPosition()
+    {
+        isReady = true;
+        objectRB.linearVelocity = Vector3.zero;
+        ObjectLaunched?.Invoke(false);
+        PositionAndReady();
     }
 }
