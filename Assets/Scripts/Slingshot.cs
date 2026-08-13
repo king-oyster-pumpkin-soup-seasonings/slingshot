1be0efd070b9a397bea93e5df72944cd2d9c062a
king-oyster-pumpkin-soup-seasonings <283332318+king-oyster-pumpkin-soup-seasonings@users.noreply.github.com>
2026-08-13 12:16:54 +0800

    feat(objects): initialize game scene objects  with platform, blocks, target, and projectile

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    private Rigidbody2D slingyRB;
+    private bool isReady;
+    private float timeCounter, timeIdlePosition;
+
     void Start()
     {
+        InitializeVariables();
+        ResetReady();
     }
 
     void Update()
     {
+    }
+
+    void InitializeVariables()
+    {
+        slingyRB = GetComponent<Rigidbody2D>();
+    }
+
+    void ResetReady()
+    {
+        slingyRB.gravityScale = 0;
+        transform.position = new Vector2(-6.75f, -2.5f);
     }
 }
